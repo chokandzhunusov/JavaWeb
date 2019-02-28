46500bb6b96e001138a9ec8e30c65b887d8e8fc6
chokandzhunusov <chokan.dzhunusov@gmail.com>
2019-02-28 12:46:23 +0600

    Actions on: 1. Added two packeges: customer, user. 2. Tried running app on local tomcat server (commented them).

--- src/main/java/com/example/myapplication/Application.java
+++ src/main/java/com/example/myapplication/Application.java
@@ -9,3 +9,22 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+//#####################################################
+//  CONFIGURATION FOR RUNNING ON LOCAL TOMCAT SERVER
+//#####################################################
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class Application  extends SpringBootServletInitializer {
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(Application.class);
+//    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
